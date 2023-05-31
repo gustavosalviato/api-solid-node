33b4340623df54c0b342dd8a9c4002bd9f0f896f
gustavosalviato <gustavosalviato8@gmail.com>
2023-05-31 15:53:51 -0300

    feat: password hashing

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
@@ -10,11 +11,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { email, name, password } = registerBodySchema.parse(request.body)
 
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
+  const password_hash = await hash(password, 6)
+
   await prisma.user.create({
     data: {
       email,
       name,
-      password_hash: password,
+      password_hash,
     },
   })
 
